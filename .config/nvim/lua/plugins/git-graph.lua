0126199b7aebb0f4c15e0632d8d06e8513cfb8a6
Zak Siddiqui <zak@kelsiem.com>
2024-11-21 22:38:17 +1000

    autoformat

--- .config/nvim/lua/plugins/git-graph.lua
+++ .config/nvim/lua/plugins/git-graph.lua
@@ -1,51 +1,51 @@
 return {
-  'isakbm/gitgraph.nvim',
-  dependencies = { 'sindrets/diffview.nvim' },
+  "isakbm/gitgraph.nvim",
+  dependencies = { "sindrets/diffview.nvim" },
   ---@type I.GGConfig
   opts = {
     symbols = {
-      merge_commit = '',
-      commit = '',
-      merge_commit_end = '',
-      commit_end = '',
-  
+      merge_commit = "",
+      commit = "",
+      merge_commit_end = "",
+      commit_end = "",
+
       -- Advanced symbols
-      GVER = '',
-      GHOR = '',
-      GCLD = '',
-      GCRD = '╭',
-      GCLU = '',
-      GCRU = '',
-      GLRU = '',
-      GLRD = '',
-      GLUD = '',
-      GRUD = '',
-      GFORKU = '',
-      GFORKD = '',
-      GRUDCD = '',
-      GRUDCU = '',
-      GLUDCD = '',
-      GLUDCU = '',
-      GLRDCL = '',
-      GLRDCR = '',
-      GLRUCL = '',
-      GLRUCR = '',
+      GVER = "",
+      GHOR = "",
+      GCLD = "",
+      GCRD = "╭",
+      GCLU = "",
+      GCRU = "",
+      GLRU = "",
+      GLRD = "",
+      GLUD = "",
+      GRUD = "",
+      GFORKU = "",
+      GFORKD = "",
+      GRUDCD = "",
+      GRUDCU = "",
+      GLUDCD = "",
+      GLUDCU = "",
+      GLRDCL = "",
+      GLRDCR = "",
+      GLRUCL = "",
+      GLRUCR = "",
     },
     format = {
       --timestamp = '%H:%M %d-%m-%Y',
-      timestamp = '%a %d %b %I:%M %p (%Y)',
-      fields = { 'hash', 'timestamp', 'author', 'branch_name', 'tag' },
+      timestamp = "  %H:%M     %b %d   󰸘 %Y     %A      ",
+      fields = { "branch_name", "hash", "timestamp", "author", "tag" },
     },
     hooks = {
       -- Check diff of a commit
       on_select_commit = function(commit)
-        vim.cmd(':DiffviewOpen ' .. commit.hash .. '^!')
-        vim.notify('DiffviewOpen ' .. commit.hash .. '^!')
+        vim.cmd(":DiffviewOpen " .. commit.hash .. "^!")
+        vim.notify("DiffviewOpen " .. commit.hash .. "^!")
       end,
       -- Check diff from commit a -> commit b
       on_select_range_commit = function(from, to)
-        vim.cmd(':DiffviewOpen ' .. from.hash .. '~1..' .. to.hash)
-        vim.notify('DiffviewOpen ' .. from.hash .. '~1..' .. to.hash)
+        vim.cmd(":DiffviewOpen " .. from.hash .. "~1.." .. to.hash)
+        vim.notify("DiffviewOpen " .. from.hash .. "~1.." .. to.hash)
       end,
     },
   },
@@ -53,7 +53,7 @@ return {
     {
       "<leader>gl",
       function()
-        require('gitgraph').draw({}, { all = true, max_count = 5000 })
+        require("gitgraph").draw({}, { all = true, max_count = 5000 })
       end,
       desc = "GitGraph - Draw",
     },
