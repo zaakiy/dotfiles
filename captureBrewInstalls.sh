4c74ccd324282d20b996c15ee0b1f18ebe0c88b1
Zak Siddiqui <zak@kelsiem.com>
2024-11-23 15:00:36 +1000

    full path

--- captureBrewInstalls.sh
+++ captureBrewInstalls.sh
@@ -1,2 +1,2 @@
-echo brew install $(brew list -l --formula | awk '{print $9}' | tr '\n' ' ') >brewInstalls.sh
-echo brew install --cask $(brew list -l --cask | awk '{print $9}' | tr '\n' ' ') >>brewInstalls.sh
+echo brew install $(brew list -l --formula | awk '{print $9}' | tr '\n' ' ') > ~/dotfiles/brewInstalls.sh
+echo brew install --cask $(brew list -l --cask | awk '{print $9}' | tr '\n' ' ') >> ~/dotfiles/brewInstalls.sh
